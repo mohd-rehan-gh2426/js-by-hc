f97579c9392e6b935619157f5bb5080ff9879e30
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-10 17:33:39 +0000

    conversion operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,56 @@
+let score = "33"
+let score1 = "33abc"
+let score2 = null
+let score3 = undefined
+let score4 = true
+let score5 = "Rehan"
+
+console.log(typeof score);
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+console.log(typeof score5);
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+
+console.log(valueInNumber);
+console.log(valueInNumber1);
+console.log(valueInNumber2);
+console.log(valueInNumber3);
+console.log(valueInNumber4);
+console.log(valueInNumber5);
+
+
+
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(typeof valueInNumber2);
+console.log(typeof valueInNumber3);
+console.log(typeof valueInNumber4);
+console.log(typeof valueInNumber5);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false; "" => false; "Rehan" => true
+
+let someNumber = 24
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
