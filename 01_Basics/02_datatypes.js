f82cd501d6cade6c425d38cb0422b30cb0af9494
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-10 16:05:50 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JSS code as newer version
+
+// alert (3+3)  //We are using nodejs not browser
+
+console.log("Rehan")
+
+let name = "Hitesh"
+let age = 21
+let isLoggedIn = false
+let state = null
+let state2
+
+console.table([name,age,isLoggedIn,state,state2]);
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
