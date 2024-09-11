66183e1b00762b60c6e3e6508383cb4b92eec694
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-11 17:00:26 +0000

    conversions

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -52,5 +52,46 @@ let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
 
+//*****************OPERATIONS ********************/
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+// let gameCounter = 100
+// let gc1 = ++gameCounter
+// console.log(gc1);
+
+
+
+
+
+
+
+
+
+
+
 
 
