92c47c1fda5461fec95065417613149117beb268
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-12 14:42:03 +0000

    Strings in javascript

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,45 @@
+const name = "Rehan"
+const repocount = 50
+
+//console.log(name + repocount + "Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const placeName = new String('Indore-Bk-An')
+// console.log(placeName[0]);
+// console.log(placeName.__proto__);
+// console.log(placeName.length);
+// console.log(placeName.toUpperCase());
+// console.log(placeName.charAt(3));
+// console.log(placeName.indexOf("o"));
+
+// const newString = placeName.substring(0, 4) //4th that is last value will not be include
+// console.log(newString);
+
+// const anotherString = placeName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne = "      Rehan     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://Rehan.com/hitesh%20chhipa"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('Rehan'));
+// console.log(url.includes('chhipa'));
+
+console.log(placeName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
