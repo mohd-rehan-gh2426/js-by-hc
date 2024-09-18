c1739dd96020b0fc94352681d9aed1aa97d66c5a
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-18 16:58:54 +0000

    dates and time

--- 01_Basics/07_dates_in_js.js
+++ 01_Basics/07_dates_in_js.js
@@ -9,14 +9,34 @@ const myDate = new Date();
 // console.log(myDate.toLocaleDateString());
 // console.log(myDate.toLocaleTimeString('en-IN'));
 // console.log(myDate.getTimezoneOffset()); // This returns the offset in minutes
-console.log(typeof myDate);
+// console.log(typeof myDate);
 
 
-let myCreateDate = new Date(2023, 0, 23)
-console.log(myCreateDate.toDateString());
+let myCreateDate = new Date("09-18-2024")
+// console.log(myCreateDate.toDateString());
 
-let myCreateDate1 = new Date(2023, 0, 23, 12 , 3)
-console.log(myCreateDate1.toLocaleString());
+// let myCreateDate1 = new Date(2023, 0, 23, 12 , 3)
+// console.log(myCreateDate1.toLocaleString());
+
+
+// let myCreateDate2 = new Date("2024-04-16")
+// console.log(myCreateDate2.toLocaleDateString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getDay())
+console.log(newDate.getMonth());
+
+
+
+
+
 
 
 
