8219779088ffc9458e1e3dc129a0469bde68b086
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-18 07:24:40 +0000

    dates and time

---
+++ 01_Basics/07_dates_in_js.js
@@ -0,0 +1,29 @@
+// Dates
+
+const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString('en-IN'));
+// console.log(myDate.getTimezoneOffset()); // This returns the offset in minutes
+console.log(typeof myDate);
+
+
+let myCreateDate = new Date(2023, 0, 23)
+console.log(myCreateDate.toDateString());
+
+let myCreateDate1 = new Date(2023, 0, 23, 12 , 3)
+console.log(myCreateDate1.toLocaleString());
+
+
+
+
+
+
+
+
+
+
