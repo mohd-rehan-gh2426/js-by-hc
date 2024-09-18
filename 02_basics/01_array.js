21cbc8ce0c66b0c5a720e438370facceab8d3848
mohd-rehan-gh2426 <119719175+mohd-rehan-gh2426@users.noreply.github.com>
2024-09-18 18:23:13 +0000

    array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,21 @@
+//  ARRAY
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myScolars = ["Alwan", "Fahad", "Khudair"]
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myScolars);
+
+// ARRAY METHODS
+
+myScolars.push("Zahrani")
+// console.log(myScolars);
+myScolars.push("Musa Sharif")
+// console.log(myScolars);
+myScolars.pop()
+// console.log(myScolars);
+myScolars.unshift("Awlaki")
+console.log(myScolars)
+myScolars.shift()
+console.log(myScolars);
+
+
